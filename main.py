2b61798906dae5939454b8de68d44eee8d4cae67
rais <juangsamiarani@gmail.com>
2024-10-08 20:19:25 +0700

    Create main.py
    bruh mewing

---
+++ main.py
@@ -0,0 +1,10 @@
+from random import choice
+character = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+lenght = int(input('how many long word?:'))
+password = ""
+
+for i in range(panjang):
+    r_c = choice(character)
+    password += kr_c
+
+print(password)
